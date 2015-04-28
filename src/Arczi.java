668086c1c6c1d2595b1619ae4e305a1a083c28ed
dirtyblacksjacks <arcio129@wp.pl>
2015-04-28 16:39:30 +0200

    Dee

---
+++ src/Arczi.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author lab716-06
+ */
+public class Arczi {
+    
+    public void wyswietlSlowo() 
+    {
+    System.out.println( "Magik" );
+    }
+            
+}
+    
+    
+
